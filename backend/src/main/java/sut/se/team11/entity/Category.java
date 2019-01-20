e7c608b24a4631e3df7717f44d6c2508f3189564
MHides <memories.cpem@gmail.com>
2019-01-20 20:24:51 +0700

    close #12 - add entity Category

---
+++ backend/src/main/java/sut/se/team11/entity/Category.java
@@ -0,0 +1,17 @@
+package sut.se.team11.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Category {
+    @Id
+    @SequenceGenerator(name = "category_seq",sequenceName = "category_seq")
+    @GeneratedValue(generator = "category_seq",strategy = GenerationType.SEQUENCE)
+    private Long categoryId;
+    private @NonNull String categoryName;
+}
