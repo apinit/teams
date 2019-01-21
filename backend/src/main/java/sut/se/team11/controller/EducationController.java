af50fc1e49129020744e3dba8ea8e3ca48f3132e
Sarida Mongkhonnum <saridam10@gmail.com>
2019-01-21 21:06:04 +0700

    add Position Controller and Education Controller - close #39

---
+++ backend/src/main/java/sut/se/team11/controller/EducationController.java
@@ -0,0 +1,25 @@
+package sut.se.team11.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sut.se.team11.entity.Education;
+import sut.se.team11.repository.EducationRopository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@CrossOrigin (origins = "http://localhost:4200")
+
+public class EducationController {
+    @Autowired
+    public EducationRopository educationRopository;
+
+    @GetMapping ("/Education")
+    public List<Education> Education() {
+        return educationRopository.findAll().stream()
+                .collect(Collectors.toList());
+    }
+}
