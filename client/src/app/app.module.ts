64f9ec19d0f17d56f02d556402b273580a8fd506
MHides <memories.cpem@gmail.com>
2019-01-21 18:41:25 +0700

    close #34 - add page layout and pre to work

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { 
+  MatButtonModule,
+  MatCardModule,
+  MatInputModule,
+  MatListModule,
+  MatToolbarModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatTableModule,
+  MatRadioModule,
+  MatCheckboxModule,
+  MatIconModule,
+  MatSidenavModule
+} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { PageLayoutComponent } from './page-layout/page-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageLayoutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatListModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatTableModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
