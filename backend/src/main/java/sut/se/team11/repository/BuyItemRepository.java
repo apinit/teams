4df21466d35ed1cf84e3b776d2c07d2821a44625
Mormook <Kultida_55@hotmail.com>
2019-01-21 22:23:41 +0700

    ทำ Controller -close #41

--- backend/src/main/java/sut/se/team11/repository/BuyItemRepository.java
+++ backend/src/main/java/sut/se/team11/repository/BuyItemRepository.java
@@ -6,5 +6,5 @@ import sut.se.team11.entity.BuyItem;
 
 @RepositoryRestResource
 public interface BuyItemRepository extends JpaRepository<BuyItem, Long> {
-    BuyItem findById(long buyItemId);
+     BuyItem findById(long buyItemId);
 }
